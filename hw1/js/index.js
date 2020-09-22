b5fca2dcb906ee2d20bbcd6e79ef90591777e4f8
Ivan Skripachev <skripachevivan@gmail.com>
2020-09-22 19:32:37 +0300

    Added hw1

--- hw1/js/index.js
+++ hw1/js/index.js
@@ -1,2 +1,15 @@
-const g = document.querySelector('g');
-const circle = document.querySelector('circle');
+function changeMood() {
+	var checked = document.getElementById("check_mood").checked;
+	if (checked)
+    {
+        document.getElementById("label_mood").innerHTML = "Happy";
+        document.getElementById("happy_path").setAttribute("visibility", "visible");
+        document.getElementById("sad_path").setAttribute("visibility", "hidden");
+    }
+    else
+    {
+        document.getElementById("label_mood").innerHTML = "Sad";
+        document.getElementById("happy_path").setAttribute("visibility", "hidden");
+        document.getElementById("sad_path").setAttribute("visibility", "visible");
+    }
+}
